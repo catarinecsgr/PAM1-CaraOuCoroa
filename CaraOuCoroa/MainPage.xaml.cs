fafbc044b812374e1f3224de3cb3b23afe35433f
Catarine <catcas08@gmail.com>
2024-04-01 22:47:43 -0300

    Cara ou coroa

---
+++ CaraOuCoroa/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace CaraOuCoroa
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SidePicker.SelectedIndex = 0;
+        }
+
+        private async void CoinFlipButton_Clicked(object sender, EventArgs e)
+        {
+            string Escolha = SidePicker.SelectedItem.ToString();
+
+            int choice = 0;
+            if (Escolha == "CARA") choice = 1;
+
+            int rnd = new Random().Next(0, 2);
+            string resultado = "CARA";
+            if(rnd == 0) resultado = "COROA";
+
+            if (rnd == choice)
+            {
+                ResultLabel.Text = $"EBA!! Deu {resultado} Você ganhou!";
+            } else
+            {
+                ResultLabel.Text = $"Deu {resultado}, você perdeu! HAHAHAH >:D";
+            }
+        }
+    }
+
+}
