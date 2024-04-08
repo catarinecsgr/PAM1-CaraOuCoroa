81aca3c84657b3be547c9d86f8d781ea7aeaa6fe
Catarine <catcas08@gmail.com>
2024-04-08 20:51:12 -0300

    insere imagem da moeda

--- CaraOuCoroa/MainPage.xaml.cs
+++ CaraOuCoroa/MainPage.xaml.cs
@@ -8,23 +8,31 @@
             SidePicker.SelectedIndex = 0;
         }
 
-        private async void CoinFlipButton_Clicked(object sender, EventArgs e)
+        private void CoinFlipButton_Clicked(object sender, EventArgs e)
         {
             string Escolha = SidePicker.SelectedItem.ToString();
 
             int choice = 0;
-            if (Escolha == "CARA") choice = 1;
-
+            if (Escolha == "COROA") choice = 1;
             int rnd = new Random().Next(0, 2);
-            string resultado = "CARA";
-            if(rnd == 0) resultado = "COROA";
-
-            if (rnd == choice)
+            string resultado = "";
+            if(rnd == 0)
             {
-                ResultLabel.Text = $"EBA!! Deu {resultado} Você ganhou!";
+                resultado = "CARA";
+                CoinImage.Source = "cara.jpg";
             } else
             {
-                ResultLabel.Text = $"Deu {resultado}, você perdeu! HAHAHAH >:D";
+                resultado = "COROA";
+                CoinImage.Source = "coroa.jpg";
+            }
+
+            if(rnd == choice)
+            {
+                ResultLabel.Text = $"Iupi!! Deu {resultado}, você ganhou!!";
+            }
+            else
+            {
+                ResultLabel.Text = $"Poxa. Deu {resultado}, você perdeu.";
             }
         }
     }
